afa9150dab871348ab038b6cf82a7a51078b8563
Olennikov Yury <olennikovyv@mail.ru>
2022-05-31 00:49:15 +0500

    Added page check to youtube site

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url) {
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
     const queryParameters = tab.url.split("?")[1];
     const urlParameters = new URLSearchParams(queryParameters);
 
     console.log(urlParameters);
+    console.log(urlParameters.get("v"));
+    console.log(urlParameters.get("t"));
 
     chrome.tabs.sendMessage(tabId, {
       type: "NEW",
